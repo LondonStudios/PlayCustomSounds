da98537d99635dac3f121b8061f6698be4a29a39
LondonStudios <64035280+LondonStudios@users.noreply.github.com>
2020-04-29 21:34:12 +0100

    Source Code
    Please see LICENCE.

---
+++ src/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using static CitizenFX.Core.Native.API;
+
+namespace Client
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            EventHandlers["Client:SoundToClient"] += new Action<string, float>(SoundToClient);
+            EventHandlers["Client:SoundToAll"] += new Action<string, float>(SoundToAll);
+            EventHandlers["Client:SoundToRadius"] += new Action<int, float, string, float>(SoundToRadius);
+            EventHandlers["Client:SoundToCoords"] += new Action<float, float, float, float, string, float>(SoundToCoords);
+        }
+
+        private void SoundToClient(string soundFile, float soundVolume)
+        {
+            SendNuiMessage(string.Format("{{\"submissionType\":\"sendSound\", \"submissionVolume\":{0}, \"submissionFile\":\"{1}\"}}", (object)soundVolume, (object)soundFile));
+        }
+
+        private void SoundToAll(string soundFile, float soundVolume)
+        {
+            SendNuiMessage(string.Format("{{\"submissionType\":\"sendSound\", \"submissionVolume\":{0}, \"submissionFile\":\"{1}\"}}", (object)soundVolume, (object)soundFile));
+        }
+
+        private void SoundToRadius(int networkId, float soundRadius, string soundFile, float soundVolume)
+        {
+            Vector3 playerCoords = Game.Player.Character.Position;
+            Vector3 targetCoords = GetEntityCoords(NetworkGetEntityFromNetworkId(networkId), true);
+            var distance = Vdist(playerCoords.X, playerCoords.Y, playerCoords.Z, targetCoords.X, targetCoords.Y, targetCoords.Z);
+            float distanceVolumeMultiplier = (soundVolume / soundRadius);
+            float distanceVolume = soundVolume - (distance * distanceVolumeMultiplier);
+            if (distance <= soundRadius)
+            {
+                SendNuiMessage(string.Format("{{\"submissionType\":\"sendSound\", \"submissionVolume\":{0}, \"submissionFile\":\"{1}\"}}", (object)distanceVolume, (object)soundFile));
+            }
+
+        }
+
+        private void SoundToCoords(float positionX, float positionY, float positionZ, float soundRadius, string soundFile, float soundVolume)
+        {
+            Vector3 playerCoords = Game.Player.Character.Position;
+            var compare = Vdist(playerCoords.X, playerCoords.Y, playerCoords.Z, positionX, positionY, positionZ);
+            float distanceVolumeMultiplier = (soundVolume / soundRadius);
+            float distanceVolume = soundVolume - (compare * distanceVolumeMultiplier);
+            if (compare <= soundRadius)
+            {
+                SendNuiMessage(string.Format("{{\"submissionType\":\"sendSound\", \"submissionVolume\":{0}, \"submissionFile\":\"{1}\"}}", (object)distanceVolume, (object)soundFile));
+            }
+        }
+
+        /*  © 2020 - London Studios - Do not modify/change source code obtained permission. 
+            This may be used on public/private FiveM servers and used in videos published to websites, 
+            This is for non-commercial use. */
+    }
+}
---
+++ src/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using static CitizenFX.Core.Native.API;
+
+namespace Server
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            EventHandlers["Server:SoundToClient"] += new Action<int, string, float>(ServerSoundToClient);
+            EventHandlers["Server:SoundToAll"] += new Action<string, float>(ServerSoundToAll);
+            EventHandlers["Server:SoundToRadius"] += new Action<int, float, string, float>(ServerSoundToRadius);
+            EventHandlers["Server:SoundToCoords"] += new Action<float, float, float, float, string, float>(ServerSoundToCoords);
+        }
+
+        private void ServerSoundToClient(int netId, string soundFile, float soundVolume)
+        {
+            Player plyr = new PlayerList()[netId];
+            plyr.TriggerEvent("Client:SoundToClient", soundFile, soundVolume);
+        }
+
+        private void ServerSoundToAll (string soundFile, float soundVolume)
+        {
+            TriggerClientEvent("Client:SoundToAll", soundFile, soundVolume);
+        }
+
+        private void ServerSoundToRadius(int netid, float soundRadius, string soundFile, float soundVolume)
+        {
+            TriggerClientEvent("Client:SoundToRadius", netid, soundRadius, soundFile, soundVolume);
+        }
+
+        private void ServerSoundToCoords(float positionX, float positionY, float positionZ, float soundRadius, string soundFile, float soundVolume)
+        {
+            TriggerClientEvent("Client:SoundToCoords", positionX, positionY, positionZ, soundRadius, soundFile, soundVolume);
+        }
+
+        /*  © 2020 - London Studios - Do not modify/change source code obtained permission. 
+            This may be used on public/private FiveM servers and used in videos published to websites, 
+            This is for non-commercial use. */
+    }
+}
