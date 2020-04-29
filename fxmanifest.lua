90bd7ee692c9eb8f6f3f5152e4929f961c7b412e
Dan <WolfKnight98@users.noreply.github.com>
2020-04-29 22:13:44 +0100

    Use globbing to load all .ogg files rather than specifying each one

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,9 +12,8 @@ ui_page 'html/index.html'
 
 files {
     'html/index.html',
-    -- List sound files:
-    -- Files must be in html/sounds/
-    -- Files must be in .ogg format
-    -- Remember to add a comma after,
-    'html/sounds/example.ogg'
+    
+    -- Sound files must be in html/sounds/
+    -- Sound files must be in .ogg format
+    'html/sounds/*.ogg'
 }
