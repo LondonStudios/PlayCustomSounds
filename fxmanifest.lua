32cb5d54663bb8e61d5f3ff77b5c29d4a4e9be9c
LondonStudios <64035280+LondonStudios@users.noreply.github.com>
2020-04-29 20:32:22 +0100

    Initial Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version '44febabe-d386-4d18-afbe-5e627f4af937'
+game 'gta5'
+
+author 'London Studios'
+description 'PlayCustomSounds'
+version '1.0.0'
+
+client_script 'Client.net.dll'
+server_script 'Server.net.dll'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    -- List sound files:
+    -- Files must be in html/sounds/
+    -- Files must be in .ogg format
+    'html/sounds/example.ogg'
+}
